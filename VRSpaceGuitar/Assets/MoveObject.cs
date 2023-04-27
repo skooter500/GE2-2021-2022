72980ac0ef7152b5cd05001ff42d8a30e0096a30
maciekgolx <65551224+maciekgolx@users.noreply.github.com>
2023-04-27 20:08:41 +0200

    comit

--- VRSpaceGuitar/Assets/MoveObject.cs
+++ VRSpaceGuitar/Assets/MoveObject.cs
@@ -4,10 +4,7 @@ using UnityEngine;
 
 public class MoveObject : MonoBehaviour
 {
-    public Transform Character;
     public Animator animator;
-    public Vector3 target = new Vector3(30,1,0);
-    public float speed=1.0f;
     // Start is called before the first frame update
     void start() 
     {
@@ -15,13 +12,12 @@ public class MoveObject : MonoBehaviour
     }
     void Update()
     {
-        StartCoroutine(MoveCube(target));
+        StartCoroutine(MoveCube());
     }
 
     // Update is called once per frame
-    IEnumerator MoveCube(Vector3 targetPosition)
+    IEnumerator MoveCube()
     {
-        transform.position= Vector3.Lerp(transform.position,targetPosition,1* Time.deltaTime);
         animator.SetTrigger("Move");
         yield return null;
     }
