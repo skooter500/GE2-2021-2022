2e683833161bbe66a475fc5abdba1dc4ce3bc7ab
maciekgolx <65551224+maciekgolx@users.noreply.github.com>
2023-04-27 23:33:00 +0200

    food comit

--- VRSpaceGuitar/Assets/SpawnerScript.cs
+++ VRSpaceGuitar/Assets/SpawnerScript.cs
@@ -18,22 +18,26 @@ public class SpawnerScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     { 
-        SpawnCharacter();
+        var NumSelect= 0;
+        NumSelect  = Random.Range(0, 9);
+        for(int i=0;i<NumSelect;i++)
+        {
+            SpawnCharacter();
+        }
     }
     public void SpawnCharacter()
     {
-        var NumSelect= 0;
-        NumSelect  = Random.Range(0, 9);
-        CharContainer.transform.position = new Vector3(-5.0f, 1.0f, 0f);
-        CharLegs = (GameObject)Instantiate(LegsPrefab[1]);
-        CharHead = (GameObject)Instantiate(HeadPrefab[1]);
-        CharArms = (GameObject)Instantiate(ArmsPrefab[1]);
-        CharChest = (GameObject)Instantiate(ChestPrefab[1]);
-        
-        CharLegs.transform.SetParent(CharContainer.transform);
-        CharHead.transform.SetParent(CharContainer.transform);
-        CharArms.transform.SetParent(CharContainer.transform);
-        CharChest.transform.SetParent(CharContainer.transform);
+            CharContainer = (GameObject)Instantiate(CharContainer);
+            CharLegs = (GameObject)Instantiate(LegsPrefab[1]);
+            CharHead = (GameObject)Instantiate(HeadPrefab[1]);
+            CharArms = (GameObject)Instantiate(ArmsPrefab[1]);
+            CharChest = (GameObject)Instantiate(ChestPrefab[1]);
+            
+            CharContainer.transform.SetParent(CharContainer.transform);
+            CharLegs.transform.SetParent(CharContainer.transform);
+            CharHead.transform.SetParent(CharContainer.transform);
+            CharArms.transform.SetParent(CharContainer.transform);
+            CharChest.transform.SetParent(CharContainer.transform);
     }
     // Update is called once per frame
     void Update()
